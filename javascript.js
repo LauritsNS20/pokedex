f6e30f3ce4907e9befd6aa8d75e139d2febf64ac
rts-6905 <U2418040@rts-365.dk>
2023-09-07 13:02:37 +0200

    javascript commit

--- javascript.js
+++ javascript.js
@@ -1,3 +1,35 @@
+fetch("https://pokeapi.co/api/v2/pokemon")
+.then(function(response) {
+  if (response.status === 200) {
+    return response.json()
+  } else {
+    document.body.innerText += "fejl ikke nogen pokemon til dig"
+  }
+})
+.then(function(data) {
+    const UL = document.querySelector(".pokemonList")
+    data.results.forEach(function(result) {
+        const LI = document.createElement("li")
+        LI.innerHTML = `<a href="/pokemon.html?name=${result.name}">${result.name}</a>`
+        UL.append(LI)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //fetch("https://pokeapi.co/api/v2/pokemon/100")
 //.them(res => res.json())
 //.them(function(result) {
@@ -5,3 +37,6 @@
    // <p>species: ${result.species.name} </p>  
    // <img src"${result.sprites.other.dream_world.front_default}" alt></img>`  
 //}) 
+
+// statuskoder 
+// 200 = success, 300 = nedirects, 400 = client errors, 500 = server errors.
