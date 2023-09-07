f5b9154ace9eb11fb6c7a7988695e04b732bae6c
rts-6905 <U2418040@rts-365.dk>
2023-09-07 08:50:22 +0200

    styling and a h1

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-fetch("https://pokeapi.co/api/v2/pokemon/100")
-.them(res => res.json())
-.them(function(result) {
-    document.body.innerHTML += `<h1>${result.name}</h1>
-    <p>species: ${result.species.name} </p>  
-    <img src"${result.sprites.other.dream_world.front_default}" alt></img>`  
-}) 
+//fetch("https://pokeapi.co/api/v2/pokemon/100")
+//.them(res => res.json())
+//.them(function(result) {
+   // document.body.innerHTML += `<h1>${result.name}</h1>
+   // <p>species: ${result.species.name} </p>  
+   // <img src"${result.sprites.other.dream_world.front_default}" alt></img>`  
+//}) 
