0acfd3cc0788ab6c68011e6e2a41ebba2a69dbe6
rts-6905 <U2418040@rts-365.dk>
2023-09-14 13:07:34 +0200

    fejlside

--- javascript.js
+++ javascript.js
@@ -3,30 +3,29 @@ const OFFSET = parseInt(URL.get("offset") || "0")
 const NEXT_PAGE  = document.querySelector(".nextPage")
 const LAST_PAGE = document.querySelector(".lastPage")
 
-
-
-
 fetch(`https://pokeapi.co/api/v2/pokemon?offset=${OFFSET}`)
-.then(function(response) {
-  if (response.status === 200) { 
-    return response.json()
-  } else {
-    document.body.innerText += "fejl ikke nogen pokemon til dig"
-  }
+  .then(function(response) { 
+  //if (response.status !== 200) 
+    throw new Error("404") 
+   return response.json()
 })
 .then(function(data) {
 
   const LAST_OFFSET = data.count - (data.count % 20) 
   NEXT_PAGE.href = `/?offset=${OFFSET >= LAST_OFFSET ? LAST_OFFSET : OFFSET + 20}`
   
-  LAST_PAGE.href = `/?offset=${OFFSET <= 0 ? 0 : OFFSET - 20}`
+  LAST_PAGE.href = `/?offset=${OFFSET <= 0 ? 0 : OFFSET - 20}` 
 
     const UL = document.querySelector(".pokemonList")
     data.results.forEach(function(result) {
         const LI = document.createElement("li")
         LI.innerHTML = `<a href="/pokemon.html?name=${result.name}">${result.name}</a>`
         UL.append(LI)
-    })
+    }) 
+})
+.catch(function(error) {
+  console.log(error)
+  window.location.href = "/ups.html"
 })
 
 
