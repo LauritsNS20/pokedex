513a716bcb534b7d81eedfd4fd222850b77bbcbc
rts-6905 <U2418040@rts-365.dk>
2023-09-14 10:55:54 +0200

    search js

---
+++ search.js
@@ -0,0 +1,22 @@
+const DATALIST = document.querySelector("#pokemonData")
+const POKEMONSEARCHBAR = document.querySelector(".pokemonSearchBar")
+
+POKEMONSEARCHBAR.addEventListener("focus", getDatalist)
+POKEMONSEARCHBAR.addEventListener("focusout", function(event) {
+POKEMONSEARCHBAR.removeEventListener("focus", getDatalist)
+})
+
+
+
+function getDatalist(event) {
+fetch("https://pokeapi.co/api/v2/pokemon?limit=10000")
+.then(function(response) {
+
+    return response.json()
+})
+.then(function(data){
+    data.results.forEach(function(pokemon) {
+        DATALIST.innerHTML += `<option>${pokemon.name}</option>`
+    })
+})
+}
